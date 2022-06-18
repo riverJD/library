1ae3b362b1624bbf7fad59d1ef6aa9ce0adb919f
river <rrivverr@protonmail.com>
2022-06-17 20:33:48 -0400

    Add footer, github link, clean up comments

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # library
-A simple library app 
+A simple library app, add your books and track which ones you have read.  Focus on clean interface.  
+
+## Instructions
+Simply click +Add to Library to add your book to the library.
+
+Toggle read status on cover. 
+
+Remove book from library with trash can icon (will prompt)
+
+Clear entire library with Reset Library button (will prompt)
+
+
+### Goal
+This project was part of The Odin Project to continue practice with DOM manipulation, 
+javascript and CSS, including grid and forms. 
+
+
+### Future
+    Persistent storage
+    Favorite author and genre tracking
--- library.js
+++ library.js
@@ -1,15 +1,13 @@
-
-// Create a library of books
-
-
+// References for repeated use
 const addBookDisplay = document.querySelector('#book-modal-container');
 const myLibrary = document.querySelector('#booklist');
 const deleteConfirmScreen = document.querySelector('#delete-book-modal');
 const resetConfirmScreen = document.querySelector('#reset-library-modal');
 
-
+// Main array to store book bojects
 let library = [];
 
+// Track and update stats for header table
 let stats = {
     totalPages: 0,
     pagesRead: 0,
@@ -19,8 +17,7 @@ let stats = {
     favAuthor: 'none',
 
     refresh: function(){
-        console.log(this.authors);
-       
+
         let statsBooks = document.querySelector('#stats-books');
         let statsBooksRead = document.querySelector('#stats-books-read');
         let statsPages = document.querySelector('#stats-pages');
@@ -32,31 +29,30 @@ let stats = {
         statsPagesRead.textContent = this.pagesRead;
     },
 
-    favAuthor: function(){
-        let tmp = [];
-        
-        
 
+    reset: function(){
+        this.totalPages = 0;
+        this.pagesRead = 0;
+        this.totalBooks = 0;
+        this.booksRead = 0;
+        this.authors = [];
     }
-
 
 }
 
 // Create constructor for book
-
 function Book(title, author, pages, type, hasRead) {
     this.title = title;
     this.author = author;
     this.pages = parseInt(pages);
     this.type = type;
     this.hasRead = hasRead;
-
-
 }
+
 
 function addBookToLibrary(book){
     
-    // Tracking for stats display
+    // Update stats
     stats.totalBooks += 1;
     stats.totalPages += book.pages;
     if (book.hasRead){
@@ -66,18 +62,13 @@ function addBookToLibrary(book){
     stats.authors.push(book.author);
     stats.refresh();
 
+
     library.push(book);
 }
 
-
-function getBookIndexFromLibrary(book){
-    return library.indexOf(book)
-}
-
-
 function removeBookFromLibrary(index){
     
-    // Tracking for stats display
+    // Update stats
     stats.totalBooks -= 1;
     stats.totalPages -= parseInt(library[index].pages);
     if (library[index].hasRead){
@@ -87,64 +78,52 @@ function removeBookFromLibrary(index){
     stats.authors.splice(`'${library[index].pages}'`, 1)
     stats.refresh(); 
     
-    // Remove book from library array (DOMs already removed)
+    // Remove book from library array
     library.splice(index, 1)
-
 }
 
+// Clear library array and DOMs to prepare for screen refresh
+// runs on every book deletion
 function clearLibrary(){
     
-    /* while (myLibrary.firstChild) {
-        myLibrary.removeChild(myLibrary.lastChild);
-    } */
     resetConfirmScreen.style.display = 'none';
     const bookstoDelete = document.querySelectorAll('.book');
     bookstoDelete.forEach(book => book.remove());
-    //library = [];
-    
 }
 
-
-
-//////// Test books /////
+//////// Test books ///////////////////////////////
 const Fellowship = new Book("Fellowship of the Ring", "J.R.R. Tolkien", 423, "Fantasy", false);
-
-const Dune = new Book("Dune", "Frank Herbert", 412, "Sci-fi", false);
-
 const OryxCrake = new Book("Oryx and Crake", "Margaret Atwood", 400, "Sci-fi", true);
 
-addBookToLibrary(Dune);
 addBookToLibrary(OryxCrake);
 addBookToLibrary(Fellowship);
+/////////////////////////////////////////////////////
 
 
-// Display entire library, useful after clearing library or on page load
+// Display entire library, after clear or page load
 function displayLibrary(){
 
     let myLibrary = document.querySelector('#booklist');
-    
+  
     for (let book in library){
-    
         displayBook(book);
     }
 
 }
 
-// Display book at newest library array index
+// Display book at last library array index
 function displayBook(libraryIndex)
 {
-    //libraryIndex == null ? book = (library.length - 1) : book = libraryIndex;
     
     if (libraryIndex == null) return console.warn('library index number required');
 
     book = libraryIndex;
 
-    console.log("Book at index is " + book + ' ' + library[book].title);
-
+    // Create a book container
     const story = document.createElement("li");
     setAttributes(story, {"class": `book`, "id": `book${book}`, "data-library-index": `${book}`});
-    
-    // Attach attributes
+
+    // Attach attributes to display
     story.appendChild(addAttributeToBook(library[book].title, 'h3'));
     
     const author = addAttributeToBook("Author: ", 'div');
@@ -159,90 +138,66 @@ function displayBook(libraryIndex)
     type.appendChild(addAttributeToBook(library[book].type, 'p'));
     story.appendChild(type);
 
-    // Create container for 'Read' and 'Delete' buttons.
+    // Create container for 'Read' and 'Delete' buttons
     const bookButtons = addAttributeToBook('Read?', 'div')
     setAttributes(bookButtons, {'class': `book-buttons }`})
-   
-
-    const hasRead = document.createElement('input');
     
-    //adds headread class to book button if
+    // Create read book toggle
+    const hasRead = document.createElement('input');
     setAttributes(hasRead, {'type': 'button', 'class': `read-button ${(library[book].hasRead) && 'hasread'}`})
     hasRead.addEventListener('click', () => {
         const bookIndex = getParentWithClass(hasRead, 'book').getAttribute('data-library-index')
+
+        // Flips read status on click
         setBookReadStatus(hasRead, !library[bookIndex].hasRead)
     });
     bookButtons.appendChild(hasRead);
 
-    // Add a button to remove book from library
+    // Create remove book popup
     const removeBtn = document.createElement("input");
     setAttributes(removeBtn, {"type": "image", "src": "./img/icons/delete.svg", "alt": "Remove book",  "class": `remove-button`, "id": `remove-book${book}`});  
     removeBtn.addEventListener('click', () => {
+       
+        deleteScreen.currentBook = story;  
         deleteConfirmScreen.style.display = "flex";
-
-        const deleteTitleText = document.querySelector('#book-to-delete')
-        
-        const index = story.getAttribute('data-library-index');
-        deleteTitleText.textContent = library[index].title;
-        
-        deleteScreen.currentBook = story;
-        
-        
-        //removeBookFromLibrary(getParentWithClass(removeBtn, 'book').getAttribute("data-library-index"))
+ 
+        const deleteTitleText = document.querySelector('#book-to-delete') 
+        const index = story.getAttribute('data-library-index');  
+        deleteTitleText.textContent = library[index].title;           
     })
+    
     bookButtons.appendChild(removeBtn);
-
     story.appendChild(bookButtons);
-
     myLibrary.appendChild(story);
 }
 
-// Toggles book readd status for card and stats
+// Toggles book read status for card
 function setBookReadStatus(button, haveRead){
     
     const index = getParentWithClass(button, 'book').getAttribute('data-library-index')
     library[index].hasRead = haveRead;
    
     if (haveRead){
-        console.log('have read')
         button.classList.add('hasread')
         stats.booksRead += 1;
         stats.pagesRead += library[index].pages;
      }
      else{
-         console.log('havent read')
          button.classList.remove('hasread');
          stats.booksRead -= 1;
          stats.pagesRead -= library[index].pages;
     
         }
     stats.refresh();
-
 }
 
-
-displayLibrary();
-stats.refresh();
-
-/* function openDeleteScreen(book){
-    function confirmDelete(book){
-        console.log(book);
-    }
-
-
-}
-*/
-
-
+// Using object to attach book marked for deletion for deletion prompt
+// First button will store book in this object, second button will call deleteBook method on it
 let deleteScreen = {
-    
-    currentBook: 'none',
-    
-    deleteBook(){
-       
 
-        console.log("deleting.. " + this.currentBook);
-        
+    currentBook: 'none',
+
+    deleteBook(){
         this.currentBook.remove();
         removeBookFromLibrary(this.currentBook.getAttribute('data-library-index'));
         clearLibrary();
@@ -250,6 +205,44 @@ let deleteScreen = {
     }
 }
 
+///////////////////
+// Helper functions
+//////////////////
+
+// Add an element to target DOM (in this case a book attribute);
+function addAttributeToBook(attributeType, targetElement, book){
+    
+    const attribute = document.createElement(`${targetElement}`);
+    attribute.textContent = attributeType;  
+    // Allow nesting if desired
+    return attribute;
+
+}
+
+// Set multible attributes at once
+function setAttributes(element, attributes)
+{
+    Object.entries(attributes).forEach(([key, value]) => {
+        element.setAttribute(key, value)
+    });
+}
+
+// Recursively search through parents for a specific DOM with specified class
+function getParentWithClass(node, nodeClass){
+
+    // Return if class not found in document
+    if (node.tagName == 'HTML'){
+        console.warn("No DOM with that class")
+        return;
+    }
+    if (node.parentNode.className == nodeClass) {
+        return(node.parentNode);
+    }
+    return(getParentWithClass(node.parentNode, nodeClass));
+}
+
+
+// Event listeners
 const confirmDeleteBook= document.querySelector('#confirm-delete');
 confirmDeleteBook.addEventListener('click', () => {
     deleteConfirmScreen.style.display = 'none';
@@ -259,7 +252,6 @@ confirmDeleteBook.addEventListener('click', () => {
 
 const addBookPopUp = document.querySelector("#add-book");
     addBookPopUp.addEventListener("click", () => {
-
         addBookDisplay.style.display = "flex";
         myLibrary.style.opacity = ".25";
     });
@@ -281,17 +273,17 @@ resetButtonPrompt.addEventListener('click', () => { resetConfirmScreen.style.dis
 })
 
 const resetButtonConfirm = document.querySelector('#confirm-reset');
-resetButtonConfirm.addEventListener('click',() => clearLibrary());
-
+resetButtonConfirm.addEventListener('click',() => {
+    stats.reset();
+    stats.refresh();
+    clearLibrary();
+});
 const resetButtonCancel = document.querySelector('#cancel-reset');
 resetButtonCancel.addEventListener('click', () => resetConfirmScreen.style.display = "none");
-
-
-const addBook = document.querySelector('form');
-
-
+/////
 
 // Create book from user input
+const addBook = document.querySelector('form');
 addBook.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(addBook);
@@ -301,51 +293,15 @@ addBook.addEventListener('submit', (e) => {
     const pages = formData.get('page_count')
     const read = formData.get('has_read')
     
-
     addBookToLibrary(new Book(title, author, pages, type, read));
     addBook.reset();
     
-    // need to subtract one from length for proper index
+    // Need to subtract 1 from library.length for proper indexing
     displayBook(library.length - 1);
 });
     
 
 
-// Helper functions
-
-// Add an element to target DOM (in this case a book attribute);
-function addAttributeToBook(attributeType, targetElement, book){
-    
-    const attribute = document.createElement(`${targetElement}`);
-    attribute.textContent = attributeType;  
-    // Allow nesting if desired
-    return attribute;
-
-}
-
-// Set multible attributes at once
-function setAttributes(element, attributes)
-{
-    Object.entries(attributes).forEach(([key, value]) => {
-        element.setAttribute(key, value)
-    });
-}
-
-// Search through parents for a specific element and return the node
-// with that class
-function getParentWithClass(node, nodeClass){
-
-    // Return if class not found in document
-    if (node.tagName == 'HTML'){
-        console.warn("No DOM with that class")
-        return;
-    }
-    
-    // return 
-    if (node.parentNode.className == nodeClass) {
-        return(node.parentNode);
-    }
-
-    return(getParentWithClass(node.parentNode, nodeClass));
-
-}
+// Initialize
+displayLibrary();
+stats.refresh();
